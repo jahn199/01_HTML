c2c53d0b846c7a129c715122fbd7cf58a2c63b49
jahn199 <anj575981@gmail.com>
2024-11-14 12:19:29 +0900

    입력된 키 이벤트 확인, 익명함수

---
+++ 시험/과제.js
@@ -0,0 +1,15 @@
+function greeting() {
+  const title = document.getElementById("title");
+  title.style.color = "green";
+}
+
+function words() {
+  const text = document.getElementsByClassName("text");
+
+  text[0].style.color = "red";
+  text[1].style.color = "red";
+  text[2].style.color = "red";
+}
+function 이름짓기() {
+  const nameOutput = document.getElementById("nameOutput");
+}
